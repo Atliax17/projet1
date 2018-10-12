1ac91aadc1d048cbd825377260f894ebf2ed938e
Corentin <corentin.medina@epsi.fr>
2018-10-12 15:05:08 +0200

    oui

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+
 
 require __DIR__.'/../vendor/autoload.php';
 
